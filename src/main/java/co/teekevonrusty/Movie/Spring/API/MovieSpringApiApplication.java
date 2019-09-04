fa84181e71fd3d167e3d38bb79bb7e360c9f46de
Orestis Sinis <orestisinis@gmail.com>
2019-09-04 10:26:46 -0400

    First Commit for Movie API

---
+++ src/main/java/co/teekevonrusty/Movie/Spring/API/MovieSpringApiApplication.java
@@ -0,0 +1,13 @@
+package co.teekevonrusty.Movie.Spring.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieSpringApiApplication.class, args);
+	}
+
+}
